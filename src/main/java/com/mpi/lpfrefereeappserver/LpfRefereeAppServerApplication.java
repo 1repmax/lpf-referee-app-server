14abc6a4eb134acaf24a59f90bfefe66dd6dd644
Raivo Lapiņš <lapins.raivo@gmail.com>
2021-11-22 10:19:56 +0200

    Initial project structure

---
+++ src/main/java/com/mpi/lpfrefereeappserver/LpfRefereeAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.mpi.lpfrefereeappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpfRefereeAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpfRefereeAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpi/lpfrefereeappserver/LpfRefereeAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpi.lpfrefereeappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpfRefereeAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
